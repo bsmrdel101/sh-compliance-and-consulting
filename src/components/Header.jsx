d2aa7a0e167a6bb80d79a020462df61c028279ca
Bennett Smrdel <fireknights318@gmail.com>
2022-10-30 20:47:09 -0500

    feat: Add header

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <header className="header">
+      <div className="header__title">
+        <h1>Affordable International Compliance Solutions</h1>
+        <h1>for the Modern Business</h1>
+      </div>
+    </header>
+  );
+}
