918f4719867f2c76aed0f038d927ea7b0e6deefa
Bennett Smrdel <fireknights318@gmail.com>
2022-10-30 22:20:20 -0500

    fix: Fix linting problems

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,13 +3,13 @@ import React from 'react';
 export default function Contact() {
   return (
     <div className="contact">
-        <h2 className="contact__title">Contact</h2>
-        <div>
-            <p className="contact__name">Ruth Hansen</p>
-            <p>(651)-964-5235</p>
-        </div>
-        <p className="contact__email">SHcomplianceconsulting@gmail.com</p>
-        <img src="/images/linkedin.png" className="contact__icon" onClick={() => window.open("https://www.linkedin.com/in/ruth-hansen-b1b5a2167/")} />
+      <h2 className="contact__title">Contact</h2>
+      <div>
+        <p className="contact__name">Ruth Hansen</p>
+        <p>(651)-964-5235</p>
+      </div>
+      <p className="contact__email">SHcomplianceconsulting@gmail.com</p>
+      <img src="/images/linkedin.png" className="contact__icon" onClick={() => window.open("https://www.linkedin.com/in/ruth-hansen-b1b5a2167/")} />
     </div>
-);
+  );
 }
