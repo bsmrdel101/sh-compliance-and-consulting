0f63cc42010ccd82c6bea5b9fa6883795ce05614
Bennett Smrdel <fireknights318@gmail.com>
2022-10-31 17:32:09 -0500

    feat: Add google analytics

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import '../styles/globals.scss';
+import { GoogleAnalytics } from 'nextjs-google-analytics';
 
 export default function Application({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <GoogleAnalytics trackPageViews />
+      <Component {...pageProps} />
+    </>
+  );
 }
